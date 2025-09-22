0ee2548135e8dee01d8c970ef03a61dcfce5a87b
AttAditya <attachment.aditya@gmail.com>
2025-09-22 14:35:03 +0530

    Dice

--- snakesladders/Dice.java
+++ snakesladders/Dice.java
@@ -1,5 +1,21 @@
 package snakesladders;
 
 public class Dice {
-  
+  private static Dice instance;
+  private Dice() {
+    if (instance != null) {
+      throw new IllegalStateException("Already instantiated");
+    }
+
+    instance = this;
+  }
+
+  public static Dice getInstance() {
+    if (instance == null) new Dice();
+    return instance;
+  }
+
+  public int roll() {
+    return (int) (Math.random() * 6) + 1;
+  }
 }
